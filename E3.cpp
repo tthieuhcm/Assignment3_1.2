c219aab7eedb666d7868ad7922c1cbd1e3f31807
Tran Trung Hieu <tthieuhcm@gmail.com>
2016-11-23 00:48:43 +0700

    Update E3.cpp

--- E3.cpp
+++ E3.cpp
@@ -1,12 +1,13 @@
 #include"Header.h"
-bool soNguyenTo(int number){
+// lấy dữ liệu, chuyển thành AVL, tính số node chẵn, lẻ, nguyên tố
+bool soNguyenTo(int number){ //hàm xác định number có phải là số nguyên tố, trả về true/false
 	if (number < 2) return false;
 	for (int i = 2; i < sqrt(number); i++){
 		if (number%i == 0) return false;
 	}
 	return true;
 };
-void preOrder(Node* root, int& chan, int& le, int& nguyento){
+void preOrder(Node* root, int& chan, int& le, int& nguyento){ // duyệt cây theo NLR
 	if (root == NULL) return;
 	if (root->data%2 == 0) chan++;
 	else le++;
@@ -23,7 +24,7 @@ void E3(){
 	ReadArrayInput("E3.txt",arr,count);
 	AVLTree tree = AVLTree::ArrayToAVL(arr,count);
 	tree.PrintAVL();
-	preOrder(tree.root,chan,le,nguyento);
+	preOrder(tree.root,chan,le,nguyento); //duyệt cây
 	cout << "So node chan la: "<< chan << "\n";
 	cout << "So node le la: "<< le << "\n";
 	cout << "So node nguyen to la: "<< nguyento << "\n";
